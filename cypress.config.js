32fc651174efd0953b33517b376f2fdb9e10797d
Luigi <130489111+luigimatchps@users.noreply.github.com>
2024-05-20 14:25:49 -0300

    install cucumber

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,23 @@
 const { defineConfig } = require('cypress');
+const { defineConfig } = require('cypress');
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
+const addCucumberPreprocessorPlugin = require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin;
+const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild');
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      on('before:browser:launch', (browser = {}, launchOptions) => {
-        prepareAudit(launchOptions);
+    async setupNodeEvents(on, config) {
+      const bundler = createBundler({
+        plugins: [createEsbuildPlugin(config)],
       });
+
+      on('file:preprocessor', bundler);
+      await addCucumberPreprocessorPlugin(on, config);
+
+      return config;
     },
+    specPattern: 'cypress/e2e/**/*.feature',
+    supportFile: 'cypress/support/e2e.js',
     experimentalSessionAndOrigin: true,
     baseUrl: //'https://staging.ernestatech.com',
     projectId: //'1p9bba',
